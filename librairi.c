adaa1e1803199d9d670e88b5ea0ea557f3c64555
zouheirrhoumri <144812016+zouheirrhoumri@users.noreply.github.com>
2024-09-04 10:01:13 +0100

    first commit

---
+++ librairi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("\t================= STOCK MANGEMENT =================");
+    printf("\n 1. Ajouter un livre au stock.");
+    printf("\n 2. Afficher tous les livres disponibles.");
+    printf("\n 3. Rechercher un livre par son titre.");
+    printf("\n 4. Mettre à jour la quantité d'un livre..");
+    printf("\n 5. Supprimer un livre du stock.");
+    printf("\n 6. Afficher le nombre total de livres en stock.");
+    printf("\n 7. EXIT");
+
+}
